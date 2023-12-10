64a6cb141f4b76ffaad19afa7db3335af0dbb2a4
Lucas Lemos Castellani <82946942+unkllc@users.noreply.github.com>
2023-12-09 23:46:09 -0300

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css';
 import Navbar from '@/components/Navbar'; // Certifique-se de que o caminho para o seu componente Navbar está correto
 import Banner from '@/components/Banner';
+import Noticias from './noticias';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar /> 
       <Banner/>
+<Noticias/>
       <Component {...pageProps} />
     </>
   );
---
+++ pages/noticias.jsx
@@ -0,0 +1,21 @@
+// Noticias.js
+import Image from 'next/image';
+import styles from '../styles/Noticias.module.css';
+
+export default function Noticias() {
+    return (
+        <>
+            <div className={styles.bgNews}>
+                <h1>Fabras News</h1>
+                <p>Notícias sobre pessoas, eventos e inovações</p>
+            </div>
+
+            
+
+
+
+        </>
+
+        
+    );
+}
